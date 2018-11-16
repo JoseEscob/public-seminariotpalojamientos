04148d1fb1d3e5255f492cc95047e4001a06b576
jescob09 <123@gmail.com>
2018-11-16 01:10:37 -0300

    se elimina comentario

--- TpAlojamientos/src/medObjects/Extra.java
+++ TpAlojamientos/src/medObjects/Extra.java
@@ -3,7 +3,6 @@ package medObjects;
 import java.util.ArrayList;
 
 public interface Extra<T>{
-	//Buenísimo
 	public void appendChild(T child);
 	public void appendChildren(ArrayList<T> children);
 	public ArrayList<T> getChildren();
