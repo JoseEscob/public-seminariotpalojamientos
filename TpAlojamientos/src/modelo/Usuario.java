6442bda2747afffe29b1308995f35cc1c853b53f
José <123@mail.com>
2018-11-18 18:35:30 -0300

    Prueba por error de sincronización..

--- TpAlojamientos/src/modelo/Usuario.java
+++ TpAlojamientos/src/modelo/Usuario.java
@@ -1,59 +1,20 @@
 package modelo;
 
-import javax.persistence.Entity;
-import javax.persistence.FetchType;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Table;
-import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.ManyToOne;
-
 import java.util.Date;
-import java.util.List;
 
-import javax.persistence.CascadeType;
-import javax.persistence.Column;
-
-@Entity
-@Table(name = "Usuario")
 public class Usuario {
-
-	@Id
-	@GeneratedValue(strategy = GenerationType.AUTO)
-	@Column(name = "idUsuario", nullable = false)
 	private int idUsuario;
 
-	// @ManyToOne(cascade = CascadeType.ALL, mappedBy="id_publicacion")
-	@ManyToOne(fetch = FetchType.EAGER)
-	@JoinColumn(name = "id_publicacion")
-	private List<Publicacion> publicaciones;
-
-	@Column(name = "nombre")
 	private String nombre;
-
-	@Column(name = "apellido")
 	private String apellido;
-
-	@Column(name = "dni")
 	private String dni;
-
-	@Column(name = "mail")
 	private String mail;
-
-	@Column(name = "fechaNac")
 	private Date fechaNac;
-
-	@Column(name = "nroUsuario")
 	private String nroUsuario;
-
-	@Column(name = "claveUsuario")
 	private String claveUsuario;
 
-	@Column(name = "puntaje")
 	private float puntaje;
 
-	@Column(name = "estadoUsuario")
 	private int estadoUsuario;
 
 	public int getIdUsuario() {
@@ -62,14 +23,6 @@ public class Usuario {
 
 	public void setIdUsuario(int idUsuario) {
 		this.idUsuario = idUsuario;
-	}
-
-	public List<Publicacion> getPublicaciones() {
-		return publicaciones;
-	}
-
-	public void setPublicaciones(List<Publicacion> publicaciones) {
-		this.publicaciones = publicaciones;
 	}
 
 	public String getNombre() {
@@ -142,14 +95,6 @@ public class Usuario {
 
 	public void setEstadoUsuario(int estadoUsuario) {
 		this.estadoUsuario = estadoUsuario;
-	}
-
-	@Override
-	public String toString() {
-		return "Usuario [idUsuario=" + idUsuario + ", publicaciones=" + publicaciones + ", nombre=" + nombre
-				+ ", apellido=" + apellido + ", dni=" + dni + ", mail=" + mail + ", fechaNac=" + fechaNac
-				+ ", nroUsuario=" + nroUsuario + ", claveUsuario=" + claveUsuario + ", puntaje=" + puntaje
-				+ ", estadoUsuario=" + estadoUsuario + "]";
 	}
 
 }
